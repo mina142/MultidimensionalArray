0eb509ffee6f0098ab5c9f14ab69c2fd03e76eae
mina142 <mina_salehi53@yahoo.com>
2019-10-25 13:37:44 -0400

    MultiDimensional array

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+       // String[][] arr = new String[12][12];
+        for (int i = 1; i <= 12; i++) {
+            for (int j = 1; j <= 12; j++) {
+
+
+                System.out.print(i*j);
+                System.out.print('\t');
+
+            }System.out.print('\n');
+        }
+    }
+}
